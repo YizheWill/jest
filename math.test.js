1a2dcaf347969d86ca53e8cfb5963342c6eba477
Yizhe Wang <yizhe87@gmail.com>
2021-02-21 20:53:05 -0800

    tobeundefined, tobenull, toEqual, tobe

--- math.test.js
+++ math.test.js
@@ -9,3 +9,22 @@ test('test minus', () => {
 test('test multi', () => {
   expect(multi(1, 2)).toBe(2);
 });
+
+// test value equals
+
+const a = { a: 1 };
+test('to equal', () => {
+  expect(a).toEqual({ a: 1 });
+});
+
+// toBeNull
+
+const b = null;
+test('to be null', () => {
+  expect(b).toBeNull();
+});
+
+const c = undefined;
+test('to be undefined', () => {
+  expect(c).toBeUndefined();
+});
